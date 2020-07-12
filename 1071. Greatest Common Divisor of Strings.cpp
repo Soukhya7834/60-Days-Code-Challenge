a32b545145d9448d8bf987bf6af9b5a4015aaa4e
Soukhya Mohan <57521056+sam7834@users.noreply.github.com>
2020-07-12 08:01:01 +0530

    Update 1071. Greatest Common Divisor of Strings.cpp

--- 1071. Greatest Common Divisor of Strings.cpp
+++ 1071. Greatest Common Divisor of Strings.cpp
@@ -1,26 +1,9 @@
 class Solution {
 public:
-    string gcdOfStrings(string str1, string str2) {
-        string res;
-        int n = str1.size();
-        for(int i =0; i<n ; i++){
-            if(str1.at(i)!= res.at(i)){
-                res += str1.at(i);
-            }
-            break;
-        }
-        string res2;
-        int m = str2.size();
-        
-        for(int i =0; i<m ; i++){
-            if(str2.at(i)!= res2.at(i)){
-                res2 += str2.at(i);
-            }
-            break;
-        }
-        if(res == res2)
-            return res;
-        else 
-            return "";
+    string gcdOfStrings(const string& s1, const string& s2)
+    {
+        return (s1 + s2 == s2 + s1)  
+		    ? s1.substr(0, gcd(size(s1), size(s2)))
+			: "";
     }
 };
