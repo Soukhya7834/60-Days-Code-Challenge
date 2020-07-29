17c867894a02e55590760a354ae5419d9388bab0
Sam <57521056+sam7834@users.noreply.github.com>
2020-07-29 19:21:52 +0530

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -16,3 +16,22 @@ public:
         return {};
     }  
 };
+
+OR
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> indices;
+        map <int, int>::iterator it;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (indices.find(target - nums[i]) != indices.end()) {
+                cout << indices[target - nums[i]] << " " << i;
+                return {indices[target - nums[i]], i};
+            }
+            indices[nums[i]] = i;
+        }
+        return {};
+    }
+};
